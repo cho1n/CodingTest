61ee886d2205928a46a3e0e624fb4199e2936e9b
cho1n <cjaewon0217@naver.com>
2023-03-24 19:09:00 +0900

    basic

--- First.py
+++ First.py
@@ -5,3 +5,12 @@ print(array)
 
 a = "How r u"
 print(a)
+
+# pop, sort
+test = [2, 3, 7, 1, 0]
+test.sort()
+print(test)
+print(test.pop())
+print(test)
+
+
---
+++ app.py
@@ -0,0 +1,7 @@
+중고차 = ['K5', 'white', '5000']
+중고차[1] = 'Black'
+
+중고차2 = { 'brand' : 'BMW','Class' : '520d'}
+중고차2['brand'] = 123
+
+print(중고차2['brand'])
