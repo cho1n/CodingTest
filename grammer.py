8b490074f273cc801bb69c54596abe2fab83ba52
cho1n <cjaewon0217@naver.com>
2023-04-04 02:57:18 +0900

    반복문

--- grammer.py
+++ grammer.py
@@ -4,9 +4,22 @@
 #     print('지금 주문가능합니다.')
 
 
-중고차재고 = ['K5', 'BMW', "Tico"]
+# 중고차재고 = ['K5', 'BMW', "Tico"]
 
-if 'K7' in 중고차재고 :
-    print('지금 주문가능합니다.') # 조건문에 in 문법 자주 사용
-else :
-    print('해당 차량이 존재하지 않습니다.')
+# if 'K7' in 중고차재고 :
+#     print('지금 주문가능합니다.') # 조건문에 in 문법 자주 사용
+# else :
+#     print('해당 차량이 존재하지 않습니다.')
+
+중고차들 = ['K5', 'BMW', 'Tico']
+
+for i in range(0,10) :
+    print('BMW 있어여')
+    
+for j in range(0,3) :
+    print(중고차들[j])
+    
+for k in 중고차들 :
+    print(k)
+
+    
