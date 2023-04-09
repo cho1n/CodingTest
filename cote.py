8c46c75de3aadd95659241e587e3e411078522ad
cho1n <cjaewon0217@naver.com>
2023-04-09 11:55:37 +0900

    len

--- cote.py
+++ cote.py
@@ -18,3 +18,14 @@ z = sorted(x, reverse=True) # 내림차순
 print(x) # [1, 3, 2, 7, 5]
 print(y) # [1, 2, 3, 5, 7]
 print(z) # [7, 5, 3, 2, 1]
+x.clear()
+print(x)
+
+a = [1, 2, 3, 4]
+len(a) # 리스트 원소의 개수 출력. 4
+
+for i in range(len(a)):
+  print(a[i])
+  
+for x in a:
+  print(x)
