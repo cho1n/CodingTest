e3c27c8dbef7b106759762cf9df6f28bc884fb05
cho1n <cjaewon0217@naver.com>
2023-04-10 19:21:55 +0900

    all,any

--- cote.py
+++ cote.py
@@ -29,3 +29,15 @@ for i in range(len(a)):
   
 for x in a:
   print(x)
+  
+  a = [11, 12, 42, 38, 7]
+# all(iterable 리스트, 튜플, 딕셔너리 등)
+# 모든 요소가 참이면 True, 하나라도 거짓이면 False
+a = [11, 12, 42, 38, 7]
+if all(60 > x for x in a):
+  print("모든 원소가 60 미만입니다.")
+
+# all(iterable 리스트, 튜플, 딕셔너리 등)
+# 요소가 하나라도 참이면 True, 전부 거짓이면 False
+if any(10 > x for x in a):
+  print("20미만인 원소가 존재합니다.")
